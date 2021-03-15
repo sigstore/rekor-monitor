6f1437d938ddc600fb608be440c0f45b8dedcd23
Santiago Torres Arias <santiago@archlinux.org>
2021-03-15 12:34:42 -0400

    *: root -> mirroring

---
+++ mirroring/test_rekorClient.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	rekorclient "github.com/sigstore/rekor-monitor/pkg"
+	"github.com/spf13/viper"
+)
+
+func _main() {
+	viper.Set("rekorServerURL", "http://0.0.0.0:3000")
+	err := rekorclient.VerifySignature()
+	if err != nil {
+		fmt.Println(err)
+
+	}
+
+}
