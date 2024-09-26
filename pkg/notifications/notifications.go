798b3189a03440ddb37cd7c64d86bb448a045678
Linus Sun <73363182+linus-sun@users.noreply.github.com>
2024-09-26 15:48:22 -0700

    implement API method to create GitHub issues notifications on found identity entries (#459)
    * add logic to parse identity entries and create monitoredIdentities for structured output
    
    Signed-off-by: linus-sun <linussun@google.com>
    
    * add omitempty tags to json
    
    Signed-off-by: linus-sun <linussun@google.com>
    
    * add support for notifications via github issue creation
    
    Signed-off-by: linus-sun <linussun@google.com>
    
    * address feedback
    
    Signed-off-by: linus-sun <linussun@google.com>
    
    ---------
    
    Signed-off-by: linus-sun <linussun@google.com>

---
+++ pkg/notifications/notifications.go
@@ -0,0 +1,34 @@
+// Copyright 2024 The Sigstore Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This file details the named fields of OID extensions supported by Fulcio.
+// A list of OID extensions supported by Fulcio can be found here:
+// https://github.com/sigstore/fulcio/blob/main/docs/oid-info.md
+// Named fields in this file have been imported from this file in the Fulcio repository:
+// https://github.com/sigstore/fulcio/blob/main/pkg/certificate/extensions.go
+// Updates to the Fulcio repository extensions file should be matched here accordingly and vice-versa.
+
+package notifications
+
+import (
+	"context"
+
+	"github.com/sigstore/rekor-monitor/pkg/identity"
+)
+
+// NotificationPlatform provides the Send() method to handle alerting logic
+// for the respective notification platform extending the interface.
+type NotificationPlatform interface {
+	Send(context.Context, []identity.MonitoredIdentity) error
+}
