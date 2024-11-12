15571c82e2bda1dc2e139211d6f976adeae6ed79
linus-sun <linussun@google.com>
2024-11-12 17:24:29 +0000

    refactor extension functions to support x509 and google_x509
    Signed-off-by: linus-sun <linussun@google.com>

--- pkg/ct/monitor_test.go
+++ pkg/ct/monitor_test.go
@@ -34,7 +34,7 @@ const (
 	organizationName = "test-org"
 )
 
-func TestScanEntryCertSubject(t *testing.T) {
+func TestScanEntrySubject(t *testing.T) {
 	testCases := map[string]struct {
 		inputEntry    ct.LogEntry
 		inputSubjects []identity.CertificateIdentity
@@ -91,7 +91,7 @@ func TestScanEntryCertSubject(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		logEntries, err := ScanEntryCertSubject(tc.inputEntry, tc.inputSubjects)
+		logEntries, err := ScanEntrySubject(tc.inputEntry, tc.inputSubjects)
 		if err != nil {
 			t.Errorf("received error scanning entry for subjects: %v", err)
 		}
